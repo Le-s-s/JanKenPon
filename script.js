ae88576ca5727aee301dd7826f0cc24b5ea5e6d8
Le-s-s <lexton.d.lewis1@gmail.com>
2025-07-31 17:26:29 -0600

    Rework to use ui

--- script.js
+++ script.js
@@ -10,32 +10,33 @@
     // whoever wins count their score up
     // when a winner is decided start a new round by counting up
 
-let round = 1;
-let player_score = 0;
-let computer_score = 0;
+document.addEventListener("DOMContentLoaded", () => {
+    let round = 1;
+    let player_score = 0;
+    let computer_score = 0;
+    const maxRound = 5;
 
-main()
-function main(){
-    let player = player_choice()
-    let computer = computer_choice()
+    // creates nodelist of buttons on page
+    const buttons = document.querySelectorAll('button');
+        //iterates through nodelist
+        buttons.forEach(button => {
 
-    let winner = play_round(player, computer)
+                // add listeners to button as it iterates over them
+                button.addEventListener('click', () => {
+                    
+                    // sets player to clicked buttons classname
+                    let player = button.className;
+                    let computer = computer_choice();
+                    let winner = play_round(player, computer);
 
-    console.log(`player chose ${player}`)
-    console.log(`computer chose ${computer}`)
-    console.log(winner)
-    console.log(`Player ${player_score} & Computer ${computer_score}`)
-    round +=1
-
-    function player_choice(){
-        let player = String(prompt("Rock, Paper, or Scissors?").toLowerCase())
-        if(player == "rock" || player == "paper" || player == "scissors"){
-            return player
-        }
-        else{
-            return player_choice()
-        }
-    }
+                    console.log(`Current round ${round}`)
+                    console.log(`player chose ${player}`);
+                    console.log(`computer chose ${computer}`);
+                    console.log(winner);
+                    console.log(`Player ${player_score} & Computer ${computer_score}`);
+                    round ++;
+                });
+        });
 
     function computer_choice(){
         const rpc = ["rock", "paper", "scissors"];
@@ -43,43 +44,41 @@ function main(){
     }
 
     function play_round(get_player_choice, get_computer_choice){
-        console.log(`Current round ${round}`)
         if (get_player_choice == get_computer_choice){
-            return "Tie"
+            return "Tie";
         }
 
         if(get_player_choice == "rock"){
             if(get_computer_choice == "paper"){
-                computer_score ++
-                return "computer wins"
+                computer_score++;
+                return "computer wins";
             }
             else{
-                player_score ++
-                return "player wins"
+                player_score++;
+                return "player wins";
             }
-
         }
 
         if(get_player_choice == "paper"){
             if(get_computer_choice == "scissors"){
-                computer_score ++ 
-                return "computer wins"
+                computer_score++; 
+                return "computer wins";
             }
             else{
-                player_score ++
-                return "player wins"
+                player_score++;
+                return "player wins";
             }
         }
 
         if(get_player_choice == "scissors"){
             if(get_computer_choice == "rock"){
-                computer_score ++
-                return "computer wins"
+                computer_score++;
+                return "computer wins";
             }
             else{
-                player_score ++
-                return "player wins"
+                player_score++;
+                return "player wins";
             }
         }
     }
-}
+});
